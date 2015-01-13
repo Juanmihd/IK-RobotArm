a67e9a3828a0209718b422e6c5363fa0466d0d3e
Juanmihd <juanmihd@gmail.com>
2015-01-13 11:08:46 +0000

    Finished dual-quaternion implementation. To start the bone-joints structures

--- dualquat.h
+++ dualquat.h
@@ -31,6 +31,10 @@ namespace octet {
       void normalize(){
         this->normalize();
       }
+      /// @brief This is to be able to access the dot product of the vector
+      float dot(const vec4 &r) const{
+        return this->dot(r);
+      }
     };
   }
 }
@@ -54,6 +58,69 @@ namespace octet{
         real_part = n_real_part;
         dual_part = n_dual_part;
       }
+      /// @brief This is the dot product with another dual-quaternion
+      /// This will receive another dual-quaternion and will return the float result
+      float dot_product(dualquat n_dualquat){
+        return this->real_part.dot(n_dualquat.real_part);
+      }
+      /// @brief Multiplicator with an scalar
+      dualquat operator*(float scalar){
+        real_part = quaternion(real_part * scalar);
+        dual_part = quaternion(dual_part * scalar);
+        return *this;
+      }
+      /// @brief This is to multiply quaternions
+      dualquat operator*(dualquat n_dualquat){
+        real_part = real_part * n_dualquat.real_part;
+        dual_part = dual_part * n_dualquat.real_part + real_part * n_dualquat.dual_part;
+      }
+      /// @brief This is to normalize a dual-quaternion
+      void normalize(){
+        float magnitudine = real_part.dot(real_part);
+        // This is to correct errors with the float point. If it's close to zero, it's zero!
+        // And therefore, if it's zero, it has to "explode"
+        assert(magnitudine > 0.000001f);
+        real_part = real_part * (1.0f / magnitudine);
+        dual_part = dual_part * (1.0f / magnitudine);
+      }
+      /// @brief This is to add quaternions
+      dualquat operator+(dualquat n_dualquat){
+        real_part = real_part + n_dualquat.real_part;
+        dual_part = dual_part + n_dualquat.dual_part;
+      }
+      /// @brief This return a new dual-quaternion that is the conjugate of the current one
+      dualquat conjugate(){
+        return dualquat(real_part.conjugate(), dual_part.conjugate());
+      }
+      /// @brief This will return a quaternion representing the rotation of the dual-quaternion
+      quaternion get_rotation(){
+        return real_part;
+      }
+      /// @brief This will return a vec3 representing the translation of the dual-quaternion
+      vec3 get_translation(){
+        quaternion translation = (dual_part * 2.0f) * real_part.conjugate();
+        return vec3(translation.get()[0], translation.get()[1], translation.get()[2]);
+      }
+      /// @brief This will return a matrix valid for octet from the dual-quaternion
+      /// As a side effect will normalize the current dual-quaternion
+      mat4t get_matrix(){
+        mat4t n_matrix;
+        // These are the elements of the quaternion that represent the rotation
+        float r_x = real_part.get()[0];
+        float r_y = real_part.get()[1];
+        float r_z = real_part.get()[2];
+        float r_w = real_part.get()[3];
+        // This is the vector that represent the translation
+        vec3 translation = get_translation();
+        // Now we combine that information and build the matrix that will represent the same information as the dual-quaternion
+        n_matrix.init(vec4( r_w*r_w + r_x*r_x - r_y*r_y - r_z*r_z,  2*r_x*r_y + 2*r_w*r_z,  2*r_x*r_z - 2*r_w*r_y,  0 ),
+                      vec4( 2*r_x*r_y - 2*r_w*r_z,  r_w*r_w + r_y*r_y - r_x*r_x - r_z*r_z,  2*r_y*r_z + 2*r_w*r_x,  0 ),
+                      vec4( 2*r_x*r_z + 2*r_w*r_y,  2*r_y*r_z - 2*r_w*r_x,  r_w*r_w + r_z*r_z - r_x*r_x - r_y*r_y,  0 ),
+                      vec4( translation.get()[0],  translation.get()[1],  translation.get()[2]  , 1 ));
+
+        return n_matrix;
+      }
     };
   }
 }
+  
