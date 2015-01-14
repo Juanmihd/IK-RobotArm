6fc94fbb3d5d098bba653dcb158a7062476c2fe6
Juanmihd <juanmihd@gmail.com>
2015-01-14 17:48:17 +0000

    Implemented the basic skeleton assembling process of the project. To DO: the Dual-Quaternion part.

--- DQ_Bone.h
+++ DQ_Bone.h
@@ -26,8 +26,17 @@ namespace octet{
     SceneNodes scene_nodes;
 
     // Adjacent bones
-    DQ_Bone* parent; // The root bone will have a NULL parent
+    ref<DQ_Bone> parent; // The root bone will have a NULL parent
     dynarray<DQ_Bone*> children; //The leaf bone will have 0 children
+
+
+    /// @brief Updates the world positions of this bone
+    /// This function assumes that its parent's joint node has updated world coordinates
+    /// This function will fix the world position and the scene_nodes of the joint and bone
+    void update_w_positions(){
+      //CODE TO DO THAT
+      //Use dual-quaternions wizzardy to obtain own leaf position
+    }
 
   public:
     /// @brief Default constructor of the Bone
@@ -43,16 +52,50 @@ namespace octet{
       printf("DQ_Bone add_scene_nodes call\n");
       scene_nodes.joint_node = n_joint_node;
       scene_nodes.bone_node = n_bone_node;
+
+      //Set up the scene nodes to their proper positions (with the Dual-Quaternion)
+    }
+
+    /// @brief Adds a parent to this bone
+    void set_parent(DQ_Bone* n_parent){
+      parent = n_parent;
     }
 
     /// @brief Adds a new child to the current bone
-    void add_child(DQ_Bone* child){}
+    void add_child(DQ_Bone* child){
+      //this bone is going to be the parent of the child bone
+      child->set_parent(this);
+      //add the children
+      children.push_back(child);
+      //fix the world positions and, then, the scene nodes
+      update_w_positions();
+    }
 
     /// @brief Update the w_positions information checking the parent and with the current dual-quaternion
     /// This function will be called by the skeleton before the draw process starts
     SceneNodes obtain_joints(){
       //Obtain the joints real world position
       return scene_nodes;
+    }
+
+    /// @brief This function will fix his joints and will ask his children to fix themselves
+    /// This is a recursion. Careful. Sokol, we are talking to you.
+    void fix_yourself(){
+      //Fix yourself
+      update_w_positions();
+      //Ask your children to fix themselves
+      for (size_t i = 0; i < children.size(); ++i)
+      {
+        //Tell each child his root position
+        children[i]->set_root_position(leaf_position);
+        //Tell him to fix himself
+        children[i]->fix_yourself();
+      }
+    }
+
+    // @brief This function returns the position of the leaf of the parent
+    void set_root_position(const vec3 &position){
+      root_position = position;
     }
   };
 }
--- DQ_Skeleton.h
+++ DQ_Skeleton.h
@@ -22,34 +22,31 @@ namespace octet{
     ref<visual_scene> app_scene;
 
     // sphere mesh set up
-    mesh_sphere* mesh_joint;
-    mesh_cylinder* mesh_bone;
+    ref<mesh_sphere> mesh_joint;
+    ref<mesh_cylinder> mesh_bone;
     dynarray<mesh_instance> skeleton_mesh_instances;
-    material* mat_joint;
-    material* mat_bone;
+    ref<material> mat_joint;
+    ref<material> mat_bone;
+    
+    // position of the skeleton
+    vec3 origin;
 
     /// @brief This function will update all the joints of the bones (starting from the root)
     void update_joints(){
       printf("DQ_Skeleton update_joints call\n");
-      // loop through bone structure recursively
-
+      // tell the root bone his root position
+      root_bone->set_root_position(origin);
+      // update current bone's joints
+      root_bone->fix_yourself();
     }
 
   public:
     /// @brief Default constructor of the DQ_Skeleton
     DQ_Skeleton(){
       printf("DQ_Skeleton Contstructor call \n");
-      root_bone = new DQ_Bone();
-      // create the default scene nodes
-      scene_node* joint_node,* bone_node;
-      joint_node = new scene_node();
-      bone_node = new scene_node();
-      // translate and rotatethe bone node by default amount. HARD CODED
-      bone_node->access_nodeToParent().translate(vec3(2.0f, 2.0f, 0.0f));
-      bone_node->access_nodeToParent().rotate(90.0f, 1.0f, 0.0f, 0.0f);
-      bone_node->access_nodeToParent().rotate(45.0f, 0.0f, 0.0f, 1.0f);
-     // bone_node->access_nodeToParent().translate(vec3(2.0f, 2.0f, 0.0f));
-      root_bone->add_scene_nodes(joint_node, bone_node);
+
+      origin = vec3(0);
+      // Initialising the meshes and materials
       // create the sphere and cylinder meshes used to draw the skeleton
       mesh_joint = new mesh_sphere();
       mesh_bone = new mesh_cylinder(zcylinder(vec3(0), 0.5f, 2.0f));
@@ -62,23 +59,32 @@ namespace octet{
     void add_scene(visual_scene* scene){
       printf("DQ_Skeleton add_scene call\n");
       app_scene = scene;
-    }
+      }
 
     /// @brief Will add a new bone to a given parent
     /// If we give no information of the parent, we asume that the new bone is the root bone
-    void add_bone(DualQuat child, DQ_Bone* parent = nullptr){
-      DQ_Bone* new_bone = new DQ_Bone(child);
+    void add_bone(const DualQuat &transform, DQ_Bone* parent = nullptr){
+      DQ_Bone* new_bone = new DQ_Bone(transform);
+      //funky code to obtain the lenght of the bone
+      float bone_lenght = 5;
+
       //Create the scene nodes
       scene_node* joint_node = new scene_node();
       scene_node* bone_node = new scene_node();
+
       //Add the scene_nodes to the new bone
       new_bone->add_scene_nodes(joint_node, bone_node);
 
-      //Here will be the code for the initialisation of the scene nodes and mesh instances
+      // add the bones_meshes to the scene
+      // we scale the bone mesh
+      app_scene->add_mesh_instance(new mesh_instance(new_bone->obtain_joints().joint_node, mesh_joint, mat_joint));
+      app_scene->add_mesh_instance(new mesh_instance(new_bone->obtain_joints().bone_node, mesh_bone, mat_bone));
 
       // Add child to the parent (or sets the root to be the new bone)
-      if (parent == nullptr)
+      if (parent == nullptr){
         root_bone = new_bone;
+        //fix root_bone world positions
+      }
       else
         parent->add_child(new_bone);
     }
@@ -89,9 +95,6 @@ namespace octet{
       printf("DQ_Skeleton draw call\n");
       // update all the joint positions of all the bones
       update_joints();
-      // add the bones to the scene
-      app_scene->add_mesh_instance(new mesh_instance(root_bone->obtain_joints().joint_node, mesh_joint, mat_joint));
-      app_scene->add_mesh_instance(new mesh_instance(root_bone->obtain_joints().bone_node, mesh_bone, mat_bone));
     }
   };
 }
