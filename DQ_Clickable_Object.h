1103cdf4ab159d30e11a9c62d9f7f9be7702556a
Sam <samhayhurst@gmail.com>
2015-01-20 19:17:43 +0000

    implement raytracing. ToDo: make bones bullet meshes

--- DQ_Clickable_Object.h
+++ DQ_Clickable_Object.h
@@ -8,7 +8,30 @@
 #ifndef DQ_CLICKABLE_OBJ_H_INCLUDED
 #define DQ_CLICKABLE_OBJ_H_INCLUDED
 
+#include "dualquat.h"
+/// @brief Objects of this class will clickable, doing so shall return a pointer to this obejct
+/// Objects of this class will have the option to be bullet physics objects.
+namespace octet{
+  class ClickableObject : public resource{
+  private:
+    DualQuat dual_quat;
+    mesh _mesh;
+    material *mat;
+    bool is_dynamic;
+    float mass;
+    btRigidBody* rigidbody;
+    ref<visual_scene> app_scene;
 
+  public:
+    ClickableObject(){}
+    ~ClickableObject(){}
+
+    /// @brief initialise the clickable object given all parameters
+    void init(visual_scene &app_scene_in, DualQuat dual_quat ){
+
+    }
+  };
+}
 
 
 #endif
